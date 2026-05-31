43e0bce313a3f9ba3e5e060fae85f229af4741bb
Tripathi_Pratyush4592 <pratyushtripathi4592@gmail.com>
2026-05-31 13:34:53 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int count =0;
+        for(int i=0; i<nums.size(); i++){
+            int temp = nums[i];
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        if(count>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
