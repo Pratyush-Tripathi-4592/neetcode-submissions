251e61d35b326bd38a943f089cbc43492b2d6ead
Tripathi_Pratyush4592 <pratyushtripathi4592@gmail.com>
2026-06-25 23:20:53 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,42 @@
+class MinStack {
+    stack<long long> st;
+    long long mini;
+
+public:
+    MinStack() {
+    }
+
+    void push(int val) {
+        if (st.empty()) {
+            st.push(val);
+            mini = val;
+        } else {
+            if (val >= mini) {
+                st.push(val);
+            } else {
+                st.push(2LL * val - mini);
+                mini = val;
+            }
+        }
+    }
+
+    void pop() {
+        if (st.empty()) return;
+
+        if (st.top() < mini) {
+            mini = 2LL * mini - st.top();
+        }
+        st.pop();
+    }
+
+    int top() {
+        if (st.top() >= mini)
+            return st.top();
+
+        return mini;
+    }
+
+    int getMin() {
+        return mini;
+    }
+};
